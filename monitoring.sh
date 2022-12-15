881a900bcccd8c747556ccc29b695f807dc7a8b0
almatos <almatos@student.42.fr>
2022-12-15 17:44:51 +0000

    up

--- monitoring.sh
+++ monitoring.sh
@@ -23,3 +23,18 @@ $'\n#Connection TCP:' `netstat -an | grep ESTABLISHED |  wc -l` \
 $'\n#User log: ' `who | cut -d " " -f 1 | sort -u | wc -l` \
 $'\nNetwork: IP ' `hostname -I`"("`ip a | grep link/ether | awk '{print $2}'`")" \
 $'\n#Sudo:  ' `grep 'sudo ' /var/log/auth.log | wc -l`
+
+
+
+
+#!/bin/bash
+cpu_virtual=$(cat /proc/cpuinfo | grep processor | wc -l)
+mem=$(free -m | awk '$1 == "Mem:" {printf("%d/%dMB (%.2f%)", $3,$2, $3/$2*100)}')
+disk_usage=$(df -h | awk '$NF=="/"{printf("%d/%dGB (%s)", $3,$2,$5)}')
+
+wall"   #Architecture: $(uname -a)
+                #CPU phisical: $(nproc)
+                #vCPU: $cpu_virtual
+                #Memory Usage: $mem
+                #Disk Usage: $disk_usage
+        "
--- myscript.sh
+++ myscript.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 cpu_virtual=$(cat /proc/cpuinfo | grep processor | wc -l)
-mem=$(free -m | awk '$1 == "Mem:" {printf("%d/%dMB (%s)"), $3,$2}')
-mem_porcent=$(free | awk '$1 == "Mem:" {printf("%.2f", $3/$2*100)}')
+mem=$(free -m | awk '$1 == "Mem:" {printf("%d/%dMB (%.2f%)", $3,$2, $3/$2*100)}')
 disk_usage=$(df -h | awk '$NF=="/"{printf("%d/%dGB (%s)", $3,$2,$5)}')
 
-wall"	#Architecture: $(uname -a)
-		#CPU phisical: $(nproc)
-		#vCPU: $cpu_virtual
-		#Memory Usage: $(mem) ($(mem_porcent))
-		#Disk Usage: $disk_usage
-	"
+wall"   #Architecture: $(uname -a)
+                #CPU phisical: $(nproc)
+                #vCPU: $cpu_virtual
+                #Memory Usage: $mem
+                #Disk Usage: $disk_usage
+        "
