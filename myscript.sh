b691803fd1fa94b7d49a21f8dd13a8423d826a24
almatos <almatos@student.42.fr>
2022-12-15 17:29:53 +0000

    up

--- myscript.sh
+++ myscript.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cpu_virtual=$(cat /proc/cpuinfo | grep processor | wc -l)
-mem_used=$(free -m | awk '$1 == "Mem:" {print $3}')
-mem_total=$(free -m | awk '$1 == "Mem:" {print $2}')
+mem=$(free -m | awk '$1 == "Mem:" {printf("%d/%dMB (%s)"), $3,$2}')
 mem_porcent=$(free | awk '$1 == "Mem:" {printf("%.2f", $3/$2*100)}')
 disk_usage=$(df -h | awk '$NF=="/"{printf("%d/%dGB (%s)", $3,$2,$5)}')
 
 wall"	#Architecture: $(uname -a)
-	#CPU phisical: $(nproc)
-	#vCPU: $cpu_virtual
-	#Disk Usage: $disk_usage
-"
+		#CPU phisical: $(nproc)
+		#vCPU: $cpu_virtual
+		#Memory Usage: $(mem) ($(mem_porcent))
+		#Disk Usage: $disk_usage
+	"
